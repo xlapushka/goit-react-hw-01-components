5c8f005b4b1b6d0890cd4ad231aacf06146fd5d8
Yuliia Hubarenko <xlapushka@ukr.net>
2023-06-30 19:10:06 +0300

    Statistics is ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,8 +13,14 @@ export const App = () => {
       stats={user.stats}
     />
 
-    {/* <Statistics title="Upload stats" stats={data} /> */}
-    <Statistics stats={data} />
+    <Statistics 
+      title={"Upload stats"} 
+      stats={data} 
+    />
+    {/* <Statistics 
+      stats={data} 
+    /> */}
+
 
   </>)
 
